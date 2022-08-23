ab764d877e6cac02cc593d8af17ab427793abbcb
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2022-08-23 00:24:18 -0300

    Project created

---
+++ src/main/java/wp/com/mongodb/mongodb/MongodbApplication.java
@@ -0,0 +1,17 @@
+package wp.com.mongodb.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping
+public class MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbApplication.class, args);
+	}
+
+}
