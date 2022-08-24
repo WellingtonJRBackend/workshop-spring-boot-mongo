cf510631f5cb55a23d78994250ce635db8aab053
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2022-08-24 10:31:48 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/wp/mongodb/config/Instantiation.java
@@ -0,0 +1,29 @@
+package wp.mongodb.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import wp.mongodb.domain.User;
+import wp.mongodb.repository.UserRepository;
+import java.util.Arrays;
+
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+    }
+}
