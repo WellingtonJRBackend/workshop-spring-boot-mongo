1fc20ab28cf5e4a5f4d573fa9b0156e5034c9d54
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2022-08-24 10:51:34 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/wp/mongodb/resources/UserResource.java
+++ src/main/java/wp/mongodb/resources/UserResource.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wp.mongodb.domain.User;
+import wp.mongodb.dto.UserDTO;
 import wp.mongodb.services.UserService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>>findAll() {
+    public ResponseEntity<List<UserDTO>>findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
