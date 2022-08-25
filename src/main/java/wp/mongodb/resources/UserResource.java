c1adbd34263f67621e9bcec2038c672152c69340
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2022-08-25 10:36:35 -0300

    Inserção de usuário com POST

--- src/main/java/wp/mongodb/resources/UserResource.java
+++ src/main/java/wp/mongodb/resources/UserResource.java
@@ -3,14 +3,13 @@ package wp.mongodb.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import wp.mongodb.domain.User;
 import wp.mongodb.dto.UserDTO;
 import wp.mongodb.services.UserService;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -31,8 +30,16 @@ public class UserResource {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<UserDTO> findById(@PathVariable String id){
-       User obj = userService.findById(id);
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = userService.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = userService.fromDTO(objDto);
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/wp/mongodb/services/UserService.java
+++ src/main/java/wp/mongodb/services/UserService.java
@@ -4,6 +4,7 @@ package wp.mongodb.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import wp.mongodb.domain.User;
+import wp.mongodb.dto.UserDTO;
 import wp.mongodb.repository.UserRepository;
 import wp.mongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,11 @@ public class UserService {
     public User findById (String id){
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+    public User insert (User obj ){
+      return userRepository.insert(obj);
+    }
+    public User fromDTO (UserDTO objDTO){
+        return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
     }
 }
