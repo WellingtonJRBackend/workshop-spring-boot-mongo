6224ff6127d696bcb54aaa1f15cb07ec0e720a11
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2022-08-25 10:50:10 -0300

    Deleção de usuário com DELETE

--- src/main/java/wp/mongodb/resources/UserResource.java
+++ src/main/java/wp/mongodb/resources/UserResource.java
@@ -42,4 +42,9 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
