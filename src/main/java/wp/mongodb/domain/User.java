baa16ce311e3de5072e859d1177ae954a1137688
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2022-08-27 11:30:28 -0300

    Referenciando os posts do usuário

--- src/main/java/wp/mongodb/domain/User.java
+++ src/main/java/wp/mongodb/domain/User.java
@@ -1,8 +1,11 @@
 package wp.mongodb.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -13,6 +16,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    List<Post> posts = new ArrayList<>();
 
     public User (){
     }
@@ -43,9 +49,16 @@ public class User implements Serializable {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
