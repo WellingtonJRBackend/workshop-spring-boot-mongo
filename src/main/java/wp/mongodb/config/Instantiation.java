87ceda1580221a80e23e6983bf9ce3ef59d2da3c
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2022-08-27 11:13:59 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/wp/mongodb/config/Instantiation.java
+++ src/main/java/wp/mongodb/config/Instantiation.java
@@ -3,11 +3,11 @@ package wp.mongodb.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import wp.mongodb.domain.AuthorDTO;
 import wp.mongodb.domain.Post;
 import wp.mongodb.domain.User;
 import wp.mongodb.repository.PostRepository;
 import wp.mongodb.repository.UserRepository;
-
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -34,10 +34,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem","vou viajar para São paulo.A braços!!",maria);
-        Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje ",maria);
-
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem","vou viajar para São paulo.A braços!!",new AuthorDTO(maria));
+        Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje ",new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1,post2));
 
     }
